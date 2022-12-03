69eab0b5acdfcd488086f7d6527f6e96dc43163b
Minoltan <minoltan.issack@arimaclanka.com>
2022-12-03 07:11:52 +0530

    New: Multiple beans connected to the context

--- spring-book/src/main/java/Main.java
+++ spring-book/src/main/java/Main.java
@@ -6,8 +6,14 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 public class Main {
     public static void main(String[] args) {
         var context = new AnnotationConfigApplicationContext(ProjectConfig.class);
-        Parrot p = context.getBean(Parrot.class);
-        System.out.println(p.getName());
+        Parrot parrot1 = context.getBean("parrot1", Parrot.class);
+        System.out.println(parrot1.getName());
+
+        Parrot parrot2 = context.getBean("parrot2", Parrot.class);
+        System.out.println(parrot1.getName());
+
+        Parrot parrot3 = context.getBean("parrot3", Parrot.class);
+        System.out.println(parrot1.getName());
 
         String s = context.getBean(String.class);
         System.out.println(s);
--- spring-book/src/main/java/ProjectConfig.java
+++ spring-book/src/main/java/ProjectConfig.java
@@ -7,11 +7,25 @@ import org.springframework.context.annotation.Configuration;
 @Configuration
 public class ProjectConfig {
     @Bean
-    Parrot parrot() {
+    Parrot parrot1() {
         var p = new Parrot();
         p.setName("Koko");
         return p;
     }
+
+    @Bean
+    Parrot parrot2() {
+        var p = new Parrot();
+        p.setName("Miki");
+        return p;
+    }
+    @Bean
+    Parrot parrot3() {
+        var p = new Parrot();
+        p.setName("Riki");
+        return p;
+    }
+
 
     @Bean
     String hello() {
